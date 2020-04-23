0b6e0d56a2e5cb82a01380f08679ccab27e7cdd6
Franchuk Petro <35275869+franchukpetro@users.noreply.github.com>
2020-04-23 14:33:01 +0300

    Column features added

--- code/main.py
+++ code/main.py
@@ -3,17 +3,31 @@ from model import MyModel
 
 # read dataset
 dataset = Dataset("training-1.csv")
+
+# create column features for further fit them into a model
+dataset.create_feature_columns()
+
 # preprocess it
 dataset.preprocess()
+
 #split on train/test
 dataset.split_data()
 
+
 # initialize model
-model = MyModel(dataset.x_train.shape[1])
+model = MyModel()
+
 # build it
-model.build()
+model.build(dataset.feature_columns)
+
 # train
-model.train(dataset.x_train, dataset.y_train)
+model.train(dataset.train_ds, dataset.val_ds)
 
 # evaluate
-model.evaluate(dataset.x_test, dataset.y_test)
+model.evaluate(dataset.test_ds)
+
+
+
+
+
+
--- code/model.py
+++ code/model.py
@@ -1,7 +1,5 @@
-from tensorflow.keras import callbacks
-from tensorflow.keras import models
+import tensorflow as tf
 from tensorflow.keras import layers
-import os
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -10,43 +8,36 @@ warnings.filterwarnings('ignore')
 
 class MyModel:
 
-    def __init__(self, input_shape):
-        self.input_shape = input_shape
-        self.model = models.Sequential()
-
-        # create folder for model weights
-        if not os.path.exists("weights"):
-            os.mkdir("weights")
-
-        #initialize checkpoints
-        checkpoint_name = 'weights/Weights-{epoch:03d}--{val_loss:.5f}.hdf5'
-        checkpoint = callbacks.ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
-        self.callbacks_list = [checkpoint]
+    def __init__(self):
+        self.model = tf.keras.Sequential()
 
 
+    def build(self, feature_columns):
+        # create layer from column features
+        feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
 
-    def build(self):
-        # The Input Layer :
-        self.model.add(layers.Dense(128, kernel_initializer='normal', input_dim=self.input_shape, activation='relu'))
-
-        # The Hidden Layers :
-        self.model.add(layers.Dense(256, kernel_initializer='normal', activation='relu'))
-        self.model.add(layers.Dense(256, kernel_initializer='normal', activation='relu'))
-        self.model.add(layers.Dense(256, kernel_initializer='normal', activation='relu'))
-
-        # The Output Layer :
-        self.model.add(layers.Dense(1, kernel_initializer='normal', activation='linear'))
-
-        # Compile the network :
-        self.model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+        self.model.add(feature_layer)
+        self.model.add(layers.Dense(128, activation='relu'))
+        self.model.add(layers.Dense(256, activation='relu'))
+        self.model.add(layers.Dense(256, activation='relu'))
+        self.model.add(layers.Dense(256, activation='relu'))
+        self.model.add(layers.Dense(1))
 
 
-    def train(self, x_train, y_train):
-        self.model.fit(x_train, y_train, epochs=500, batch_size=32, validation_split=0.2, callbacks=self.callbacks_list)
+        self.model.compile(loss='mean_absolute_error',
+                      optimizer='adam',
+                      metrics=['mean_absolute_error'])
 
 
-    def evaluate(self, x_test, y_test):
-        results = self.model.evaluate(x_test, y_test, batch_size=32)
+    def train(self, train_ds, val_ds):
+        self.model.fit(train_ds,
+                  validation_data=val_ds,
+                  epochs=100)
+
+
+
+    def evaluate(self, test_ds):
+        results = self.model.evaluate(test_ds, batch_size=32)
         print("Test loss: {}\nTest accuracy: {}".format(results[0], results[1]))
 
 
